3bb31cc906eb80540aa89f60dc0c549b3b1691f1
DePreeManley <ps26dmanley@efcts.us>
2025-05-05 10:08:44 -0400

    changes

---
+++ SteelBallRun.js
@@ -0,0 +1,24 @@
+document.addEventListener('click', event =>{
+    const header = event.target.closest('.click')
+    if (!header) return
+    
+    const openItem = document.querySelector('click.active')
+    
+    if (openItem && openItem !==header) {
+        openItem.classList.remove('active')
+        openItem.nextElementSibling.computedStyleMap.maxHeight = null;
+        openItem.querySelector('.icon').src = './res/images/plus.png'
+    }
+    
+    header.classList.toggle('active')
+    const content = header.nextElementSibling;
+    const icon = header.querySelector('.icon')
+    
+    if (header.classList.contains('active')){
+        content.style.maxHeight = content.scrollHeight + 'px'
+        icon.src = './res/images/minus_PNG55.png'
+    } else{
+        content.style.maxHeight = null;
+        icon.src = './res/images/plus.png'
+    }
+    })
