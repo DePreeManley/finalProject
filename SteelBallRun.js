ab24c0c7308aaf960cbc7d2499752b24c856795a
DePreeManley <ps26dmanley@efcts.us>
2025-05-12 09:01:21 -0400

    fixed card bug

--- SteelBallRun.js
+++ SteelBallRun.js
@@ -2,9 +2,10 @@ document.addEventListener('click', event =>{
     const header = event.target.closest('.BTTN')
     if (!header) return
     
-    const openItem = document.querySelector('BTTN.active')
-    
+    const openItem = document.querySelector('.BTTN.active')
+
     if (openItem && openItem !==header) {
+        
         openItem.classList.remove('active')
         openItem.nextElementSibling.computedStyleMap.maxHeight = null;
         openItem.querySelector('.icon').src = './res/images/plus.png'
@@ -12,9 +13,12 @@ document.addEventListener('click', event =>{
     
     header.classList.toggle('active')
     const content = header.nextElementSibling;
+
+   
     const icon = header.querySelector('.icon')
-    
+
     if (header.classList.contains('active')){
+        
         content.style.maxHeight = content.scrollHeight + 'px'
         icon.src = './res/images/minus_PNG55.png'
     } else{
